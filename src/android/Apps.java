1a8d0b3b573dd28bda180563bfb2283986827933
Danyzzz <android.developer@libero.it>
2015-10-23 14:18:40 +0200

    Update Apps.java

--- src/android/Apps.java
+++ src/android/Apps.java
@@ -36,7 +36,7 @@ public class Apps extends CordovaPlugin {
         ArrayList<String> list  = new ArrayList<String>();
         for (ResolveInfo resolve : resovleInfos) {
             String packageName = resolve.activityInfo.packageName;
-            String strAppName  = resolve.activityInfo.applicationInfo.publicSourceDir.toString();
+            String strAppName  = resolve.activityInfo.applicationInfo.loadLabel(packageMgr).toString();
             list.add(packageName);
             list.add(strAppName);
         }
