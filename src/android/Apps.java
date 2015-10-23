ed69602683b896a31fd11eafc4e54a960dfd2c25
Danyzzz <android.developer@libero.it>
2015-10-24 00:27:45 +0200

    Update Apps.java

--- src/android/Apps.java
+++ src/android/Apps.java
@@ -27,43 +27,22 @@ public class Apps extends CordovaPlugin {
         ctx = cordova.getActivity().getApplicationContext();
     }
 
-    class PInfo {
-        private String appname = "";
-        private String pname = "";
-        private String versionName = "";
-        private int versionCode = 0;
-        private Drawable icon;
-        private void prettyPrint() {
-            Log.v(appname + "   " + pname + "   " + versionName + " " + versionCode);
-        }
-    }
+    private JSONArray list() {
+        PackageManager packageMgr = ctx.getPackageManager();
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> resovleInfos = packageMgr.queryIntentActivities(mainIntent, 0);
 
-    private ArrayList<PInfo> getPackages() {
-        ArrayList<PInfo> apps = getInstalledApps(false); /* false = no system packages */
-        final int max = apps.size();
-        for (int i=0; i<max; i++) {
-            apps.get(i).prettyPrint();
+        ArrayList<String> list  = new ArrayList<String>();
+        for (ResolveInfo resolve : resovleInfos) {
+            String packageName = activityInfo.packageName;
+            String strAppName  = activityInfo.loadLabel(packageMgr).toString();
+            list.add(packageName);
+            list.add(strAppName);
         }
-        return apps;
-    }
-
-    private ArrayList<PInfo> getInstalledApps(boolean getSysPackages) {
-        ArrayList<PInfo> res = new ArrayList<PInfo>();        
-        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
-        for(int i=0;i<packs.size();i++) {
-            PackageInfo p = packs.get(i);
-            if ((!getSysPackages) && (p.versionName == null)) {
-                continue ;
-            }
-            PInfo newInfo = new PInfo();
-            newInfo.appname = p.applicationInfo.loadLabel(getPackageManager()).toString();
-            newInfo.pname = p.packageName;
-            newInfo.versionName = p.versionName;
-            newInfo.versionCode = p.versionCode;
-            newInfo.icon = p.applicationInfo.loadIcon(getPackageManager());
-            res.add(newInfo);
-        }
-        return new JSONArray(res);
+        List<String> ulist = new ArrayList<String>(new HashSet<String>(list));
+        
+        return new JSONArray(ulist);
     }
 
     /**
